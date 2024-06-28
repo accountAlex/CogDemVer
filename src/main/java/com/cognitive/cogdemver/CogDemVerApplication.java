3807d173f56fa7f42bdb9ebc0b90ecbc67d14c61
alexanderzarechkin <alexander.zarechkin@yandex.ru>
2024-06-28 12:07:27 +0300

    start

---
+++ src/main/java/com/cognitive/cogdemver/CogDemVerApplication.java
@@ -0,0 +1,13 @@
+package com.cognitive.cogdemver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogDemVerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CogDemVerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cognitive/cogdemver/CogDemVerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognitive.cogdemver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogDemVerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
