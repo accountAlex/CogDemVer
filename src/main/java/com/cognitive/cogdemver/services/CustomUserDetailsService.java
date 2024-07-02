5ae7601908d783f0e1fe26308f351b88a17b5f4d
alexanderzarechkin <alexander.zarechkin@yandex.ru>
2024-07-03 02:32:07 +0300

    ебучая регистрация и авторизация + React

--- src/main/java/com/cognitive/cogdemver/services/CustomUserDetailsService.java
+++ src/main/java/com/cognitive/cogdemver/services/CustomUserDetailsService.java
@@ -1,6 +1,6 @@
 package com.cognitive.cogdemver.services;
 
-import com.cognitive.cogdemver.entities.User;
+import com.cognitive.cogdemver.models.User;
 import com.cognitive.cogdemver.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
